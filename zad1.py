fac21eab98c4da6e171fc255edce373248c48c0b
gglaba <72909829+gglaba@users.noreply.github.com>
2020-11-25 21:28:35 +0100

    Delete zad1.py

--- zad1.py
+++
@@ -1,5 +0,0 @@
-a=input()
-b=input()
-suma= a+b
-print(suma)
-#po podstawieniu za a 8 a za b 3 otrzymałem sumę 83,wynika to z roznicy pomiedzy pythonem3 a pythonem2 w ktorym sume dostalbym poprawna czyli 11,python3 traktuje dane z inputów jako string danych,natomiast python2  "wie" ze sa to liczby.
