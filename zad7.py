6df1a5b11759fa4b7b783188bb662b2d785cfbcd
gglaba <72909829+gglaba@users.noreply.github.com>
2020-11-25 21:26:15 +0100

    Delete zad7.py

--- zad7.py
+++
@@ -1,5 +0,0 @@
-#zmienna _ jest odpowiednikiem zmiennej ans,tzn. przechowuje ona wynik poprzedniej operacji,mozna jej wartosc również przypisać do innej zmiennej utworzej przez nas np.,
-2+2
-_ 
-_+5
-a=_
